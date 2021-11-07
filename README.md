0b31c14293121161b1373f8844282716a59a3faa
Kostandin Tahiri <kostandin.tahiri@klarna.com>
2021-11-07 20:42:35 +0100

    Split JS code, update README

--- README.md
+++ README.md
@@ -1,8 +1,14 @@
+1. 
 Install project dependencies:
-`npm i`
+`npm install`
 
-Run webpack for bundling code (in watch mode):
-`npm run watch`
+2. 
+- Either:
+    - Run webpack for bundling code (in watch mode):
+    `npm run watch`
 
-Run webserver to render project in browser:
-`npm run server`
+    - Run webserver to render project in browser:
+    `npm run server`
+
+- Or:
+`npm run watch & npm run server`
---
+++ src/initUtil.js
@@ -0,0 +1,34 @@
+const BLUE = "#33ccff";
+
+export function setNodes(nodes, g) {
+    for (let i = 0; i < nodes.length; i++) {
+        var node = nodes[i];
+        g.nodes.push({
+            id: node["id"],
+            label: node["title"],
+            x: node["x"],
+            y: node["y"],
+            size: 20,
+            color: node["colour"]
+        });
+    }
+    return nodes;
+}
+
+export function setEdges(nodes, g) {
+    for (let i = 0; i < nodes.length; i++) {
+        var node = nodes[i];
+        node["connecting_nodes"].forEach(connecting_node => connectNodes(node["id"], connecting_node, g));
+    };
+}
+
+export function connectNodes(firstNodeId, secondNodeId, g) {
+    g.edges.push({
+        id: 'e' + firstNodeId + secondNodeId,
+        source: firstNodeId,
+        target: secondNodeId,
+        size: 30,
+        color: BLUE,
+        type: 'line'
+    });
+}
