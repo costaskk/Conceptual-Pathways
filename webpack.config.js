07c0a28956f00bb2c9a7878c206814f0f5acf3eb
Kostandin Tahiri <kostandin.tahiri@klarna.com>
2022-01-18 22:52:52 +0100

    Fix failing builds with config

--- webpack.config.js
+++ webpack.config.js
@@ -8,7 +8,8 @@ module.exports = {
             use: [
                 'style-loader',
                 'css-loader',
-            ]
+                'sass-loader'
+            ],
         },
       ],
     },
